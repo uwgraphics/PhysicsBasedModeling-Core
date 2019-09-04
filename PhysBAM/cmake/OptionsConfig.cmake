dbc65b8e93b1a3d69fcc82aba06d28dc6c0adb8b
Eftychios Sifakis <eftychios.sifakis.-nd@disneyresearch.com>
2019-09-04 15:09:02 -0500

    Initial Version

---
+++ PhysBAM/cmake/OptionsConfig.cmake
@@ -0,0 +1,20 @@
+  set(compile_id_types_as_int 1)
+  set(compile_without_dyadic_support 1)
+  set(compile_without_rle_support 1)
+  set(compile_without_zlib_support 1)
+  set(use_pthreads 0)
+
+  set(use_opengl 0)
+  set(use_rendering 0)
+
+# USE_COMPRESSION=0
+# USE_FFMPEG=0
+# USE_LIBJPEG=0
+# USE_LIBPNG=0
+# USE_FLTK=0
+# USE_OPENEXR=0
+# USE_OPENVDB=0
+# USE_PTHREADS=1
+# compile_without_double_support=0
+# USE_MPI=1
+# USE_LAM=0
